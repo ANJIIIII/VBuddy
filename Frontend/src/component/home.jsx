e088ec41bb7d13610e658962721e77ad52b3ecf4
Anjali <ANJALIMISHRAPPS@GMAIL.COM>
2025-06-19 07:44:45 +0530

    home

--- Frontend/src/component/home.jsx
+++ Frontend/src/component/home.jsx
@@ -1,26 +1,12 @@
 import React from 'react';
-import HeroSection from "../HomepageComponent/HeroSection"
-import PromiseSection from "../HomepageComponent/PromiseSection"
-import WhyRelySection from "../HomepageComponent/Whyrely"
-import ServicesSection from "../HomepageComponent/Services"
-import BookingSection from "../HomepageComponent/BookingSection"
-import FAQSection from "../HomepageComponent/FaqSection"
-import Footer from "../HomepageComponent/Footer"
-import InstagramReelsGrid from "../HomepageComponent/instareel";
+
 
 
 
 const home = () => {
   return (
     <div>
-       <HeroSection />
-        <PromiseSection />
-        <WhyRelySection />
-        <ServicesSection />
-        <InstagramReelsGrid/>
-        <BookingSection />
-        <FAQSection />
-        <Footer />
+       <p>hiiii</p>
     </div>
   )
 }
